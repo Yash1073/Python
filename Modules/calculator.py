abd3de8103af6fc6adc66ba36d0aa2ab222e5337
Yash Parmar <yashparmar1073@gmail.com>
2024-02-29 18:45:15 +0530

    M

---
+++ Modules/calculator.py
@@ -0,0 +1,12 @@
+CONSTANT="Yash Parmar"
+class A:
+    pass
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a//b
---
+++ Modules/main.py
@@ -0,0 +1,11 @@
+import calculator
+
+print(calculator.add(1,2))
+print(calculator.mul(1,2))
+print(calculator.sub(1,2))
+print(calculator.div(4,2))
+
+
+
+a=calculator.A()
+print(type(a))
