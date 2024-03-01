d1418b4f735778fcae36febd5f5fe6924001b601
Yash Parmar <yashparmar1073@gmail.com>
2024-03-01 11:07:51 +0530

    Import & use module

---
+++ python-06(Package)/main.py
@@ -0,0 +1,6 @@
+# from math1 import simple
+from math1.simple  import sub   
+
+# print(simple.add(1,2))
+# print(complex.cube(2))
+print(sub(4,2))
---
+++ python-06(Package)/math1/simple.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a//b
+
+
+
+print(__name__)
+if __name__=='__main__':
+    print("Hello")
+    a=int(input())
+    b=int(input())
+    print(a+b)
